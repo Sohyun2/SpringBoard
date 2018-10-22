dc405fd764aa9f8d3653f66bcddbd4fa708e99bb
Lee Sohyun <olsh1108o@daum.net>
2018-10-22 16:55:48 +0900

    ex02

--- ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -17,8 +18,62 @@ public class BoardMapperTests {
 	@Setter(onMethod_ = {@Autowired})
 	private BoardMapper mapper;
 	
-	@Test
-	public void testGetList() {
-		mapper.getList().forEach(board->log.info(board));
-	}
+//	@Test
+//	public void testGetList() {
+//		mapper.getList().forEach(board->log.info(board));
+//	}
+	
+//	@Test
+//	public void testInsert() {
+//		BoardVO board = new BoardVO();
+//		board.setTitle("새로 작성하는 글");
+//		board.setContent("새로 작성하는 내용");
+//		board.setWriter("newbie");
+//		
+//		mapper.insert(board);
+//		
+//		log.info(board);
+//	}
+	// C
+//	@Test
+//	public void testInsertSelectKey() {
+//		BoardVO board = new BoardVO();
+//		board.setTitle("새로 작성하는 글 select key");
+//		board.setContent("새로 작성하는 내용 select key");
+//		board.setWriter("newbie");
+//		
+//		mapper.insertSelectKey(board);
+//		
+//		log.info(board);
+//	}
+//	
+//	// R
+//	@Test
+//	public void testRead() {
+//		BoardVO board = mapper.read(3L);
+//		
+//		log.info(board);
+//	}
+//	
+//	// U
+//	@Test
+//	public void testUpdate() {
+//		BoardVO board = new BoardVO();
+//
+//		board.setBno(1L);
+//		board.setTitle("hello world");
+//		board.setContent("hello~");
+//		board.setWriter("lee");
+//		
+//		int count = mapper.update(board);
+//		log.info("UPDATE COUNT : " + count);
+//		
+//	}
+//	
+//	// D
+//	@Test
+//	public void testDelete() {
+//		log.info("DELETE COUNT : " + mapper.delete(3L));
+//	}
+	
 }
